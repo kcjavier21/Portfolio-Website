698d95c0878039b76c5bc403407072628ab0d874
Ken Javier <64310834+kcjavier21@users.noreply.github.com>
2020-05-13 18:27:07 +0800

    Updated 'Home' section

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
-/*app.get('/', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'portfolio.html'));
-});*/
+});
 
 const PORT = process.env.PORT || 3000;
 
